9dded45ca11703b0feb00c5539a9259abbab4343
Burcu ŞENOL <burcusenol62@gmail.com>
2021-04-03 18:06:56 +0300

    03.04.2021

--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -7,7 +7,7 @@ using System.Text;
 namespace Entities.Concrete
 {
     
-    public class Customer:User,IEntity
+    public class Customer:IEntity
     {
         public int CustomerId { get; set; }
         public int UserId { get; set; }
